955a47943bace56f69ce30467661458c7e87a3df
Ilya N. Zykin <zykin-ilya@ya.ru>
2012-05-07 15:47:45 +0400

    comments fin

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,4 +12,10 @@ class CommentsController < ApplicationController
   def comments
     @comments = Comment.nested_set.all
   end
+
+  def create
+    @comment = Comment.new params[:comment]
+    @comment.save!
+    redirect_to(request.referer || '/')
+  end
 end
